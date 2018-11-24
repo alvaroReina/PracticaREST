937fa9599eb3d1e6f912d518a56c64a3bac6516b
Alvaro Reina <arb952@gmail.com>
2018-11-25 00:07:37 +0100

    Añadido CORS

--- B4servidorREST/src/java/service/ApplicationConfig.java
+++ B4servidorREST/src/java/service/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(config.CrossOriginResourceSharingFilter.class);
         resources.add(service.SerieFacadeREST.class);
         resources.add(service.SketchFacadeREST.class);
     }
