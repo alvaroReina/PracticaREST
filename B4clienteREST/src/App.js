62727ed95caf6bcfec3d356149d0949cd7db0ecc
José Vega Moreno <jvegamoreno@outlook.com>
2018-11-27 11:44:52 +0100

    Prueba Grid Lista Series
    Con popovers para editar, ver y eliminar

--- B4clienteREST/src/App.js
+++ B4clienteREST/src/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import GridSeries from './components/GridSeries';
+
+const data = [
+  {id: 5,name: 'Doraemon', author: 'Luke international', img:'https://estaticos.elperiodico.com/resources/jpg/8/0/1481901283808.jpg', score:5 },
+  {id: 6,name: 'SpiderMan', author: 'Marvel', img: 'https://i.blogs.es/26b566/sps4/450_1000.png', score:6 },
+  {id: 7,name: 'Scooby Doo', author: 'Warner Brothers', img: 'https://static.tvtropes.org/pmwiki/pub/images/new_scoow_doow_7704.png' , score:7 },
+  {id: 8,name: 'Soldado de invierno', author: 'Marvel', img: 'https://cdn.normacomics.com/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/i/m/image_gallery_1__5.jpg', score:8 },
+  {id: 9,name: 'Shizuka', author:'Luke international', img: 'https://vignette.wikia.nocookie.net/doblaje/images/3/3e/Shizuka.png/revision/latest?cb=20130707175241&path-prefix=es', score:9 },
+  {id: 10,name: 'Pepe the froggo', author: 'Fido inc', img: 'https://vignette.wikia.nocookie.net/universe-of-smash-bros-lawl/images/5/57/Pepe.png/revision/latest?cb=20160317200950', score:10 },
+
+]
 
 class App extends Component {
   render() {
@@ -8,6 +19,7 @@ class App extends Component {
         <Button variant="contained" color="primary">
           Hello world
         </Button>
+        <GridSeries series={data} currentUser={"Marvel"}/>
       </div>
     );
   }
---
+++ B4clienteREST/src/components/SeriePopover.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/Button';
+import Popover from '@material-ui/core/Popover';
+import MoreVertRounded from '@material-ui/icons/MoreVertRounded';
+import {Button} from '@material-ui/core';
+
+
+const styles = theme => ({
+    icon: {
+        color: 'rgba(255,255,255,0.85)',
+        backgroundColor: 'rgba(0,0,0,0.35)'
+    },
+})
+
+class SeriePopover extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            anchorEl: null,
+        }
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+        this.edit = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
+        this.view = this.view.bind(this);
+    }
+
+    open(event) {
+        this.setState({
+            anchorEl: event.currentTarget,
+          });
+    }
+
+    close() {
+        this.setState({
+            anchorEl: null,
+        });
+    }
+
+    edit() {
+        let {serie} = this.props;
+        alert(`Editando ${serie.name}`)
+        this.close()
+    }
+
+    remove() {
+        let {serie} = this.props;
+        alert(`Eliminando ${serie.name}`)
+        this.close()
+    }
+
+    view() {
+        let {serie} = this.props;
+        alert(`Navegando a ${serie.name}`)
+        this.close()
+    }
+
+    render() {
+        const { classes, isOwner } = this.props;
+        const { anchorEl } = this.state;
+        let open = Boolean(anchorEl);
+
+        return (
+            <div>
+                <IconButton
+                    variant='text'
+                    aria-owns={open ? 'simple-popper' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.open}
+                    className={classes.icon}>
+                    <MoreVertRounded />
+                </IconButton>
+                <Popover
+                    id="simple-popper"
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={this.close}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                >
+                    <Button onClick={this.view} color='primary' variant='text'>
+                        View
+                    </Button>
+                    {isOwner && <Button onClick={this.edit} color='default' variant='text'>Edit</Button>}
+                    {isOwner && <Button onClick={this.remove} color='secondary' variant='text'>Remove</Button>}
+                </Popover>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(SeriePopover);
