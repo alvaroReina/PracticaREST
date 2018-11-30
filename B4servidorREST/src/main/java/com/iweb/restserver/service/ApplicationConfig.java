b65f2fc1bcb33be4e04d5a512f1e2dc83c46e223
José Vega Moreno <jvegamoreno@outlook.com>
2018-11-30 13:10:08 +0100

    CORST & SECRET

--- B4servidorREST/src/main/java/com/iweb/restserver/service/ApplicationConfig.java
+++ B4servidorREST/src/main/java/com/iweb/restserver/service/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.iweb.restserver.config.CORSFilter.class);
         resources.add(com.iweb.restserver.security.AuthenticationFilter.class);
         resources.add(com.iweb.restserver.service.SerieFacadeREST.class);
         resources.add(com.iweb.restserver.service.VinietaFacadeREST.class);
