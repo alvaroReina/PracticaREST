ebacff299c9aa73bc8ab644a9fa4e9b1859d38b4
José Vega Moreno <jvegamoreno@outlook.com>
2018-12-01 19:24:35 +0100

    NoMaven Removed
    And one method to get author by id

--- B4servidorREST/src/main/java/com/iweb/restserver/service/UserinfoFacadeREST.java
+++ B4servidorREST/src/main/java/com/iweb/restserver/service/UserinfoFacadeREST.java
@@ -5,13 +5,18 @@
  */
 package com.iweb.restserver.service;
 
+import com.google.j2objc.annotations.AutoreleasePool;
 import com.iweb.restserver.entity.Userinfo;
+import com.iweb.restserver.security.RequireAuthentication;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -19,7 +24,6 @@ import javax.ws.rs.core.MediaType;
  * @author jose
  */
 @Stateless
-@Path("signin")
 public class UserinfoFacadeREST extends AbstractFacade<Userinfo> {
 
     @PersistenceContext(unitName = "com.iweb_B4servidorREST_war_1.0-SNAPSHOTPU")
@@ -30,11 +34,19 @@ public class UserinfoFacadeREST extends AbstractFacade<Userinfo> {
     }
 
     @POST
+    @Path("signin")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public void signin(Userinfo entity) {
         super.create(entity);
     }
 
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Path("users/{id}")
+    @RequireAuthentication
+    public Userinfo findByID(@PathParam("id") Integer id) {
+        return super.find(id);
+    }
 
     @Override
     protected EntityManager getEntityManager() {
