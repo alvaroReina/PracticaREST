2e1dc79b986c8deb1b16b42edaa33ce987b43a65
felix13 <felix13moreno@gmail.com>
2018-12-10 12:30:28 +0100

    merge

--- B4servidorREST/src/main/java/com/iweb/restserver/service/ApplicationConfig.java
+++ B4servidorREST/src/main/java/com/iweb/restserver/service/ApplicationConfig.java
@@ -29,11 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.iweb.restserver.config.CORSFilter.class);
-        resources.add(com.iweb.restserver.security.AuthenticationFilter.class);
-        resources.add(com.iweb.restserver.service.SerieFacadeREST.class);
-        resources.add(com.iweb.restserver.service.SketchFacadeREST.class);
-        resources.add(com.iweb.restserver.service.UserinfoFacadeREST.class);
     }
     
 }
