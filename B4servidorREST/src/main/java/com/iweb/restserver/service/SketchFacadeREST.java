e0c4cfa674f7b5145070829ded77891c4a2c8750
mj27395 <mj27395@192.168.1.37>
2018-12-10 16:40:05 +0100

    editar sketches con autenticacion

--- B4servidorREST/src/main/java/com/iweb/restserver/service/SketchFacadeREST.java
+++ B4servidorREST/src/main/java/com/iweb/restserver/service/SketchFacadeREST.java
@@ -24,9 +24,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  *
@@ -47,54 +49,58 @@ public class SketchFacadeREST extends AbstractFacade<Sketch> {
     @Override
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response create(Sketch entity) {
-        
+
         RestResponse resp = new RestResponse(true);
-        
-        if (entity.getIdserie() == null && "".equals(entity.getTitle())){            
+
+        if (entity.getIdserie() == null && "".equals(entity.getTitle())) {
             return newError(BAD_REQUEST, "IDSERIE or title not present", null, "Please, insert IDSERIE or title first").build();
         }
-        
+
         entity.setCreatedat(new Date(System.currentTimeMillis()));
-        entity.setScore(5);        
+        entity.setScore(5);
         super.create(entity);
-        
+
         resp.isSuccessful(true)
-                    .withStatus(Response.Status.OK);        
-               
+                .withStatus(Response.Status.OK);
+
         return resp.build();
-        
+
     }
 
     @PUT
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response edit(@PathParam("id") Integer id, Sketch entity) {
-        
+    public Response edit(@PathParam("id") Integer id, Sketch entity, @Context SecurityContext sc) {
+
         RestResponse resp = new RestResponse(true);
-        
-        if (id == null){
+
+        if (!sc.isUserInRole("ADMIN") && !sc.getUserPrincipal().getName().equals(entity.getIdserie().getAuthor().getEmail())) {
+            return ResponseFactory.newError(Response.Status.FORBIDDEN, "you cannot access to this resource").build();
+        }
+
+        if (id == null) {
             return newError(BAD_REQUEST, "ID not present", null, "Please, insert ID first").build();
         }
         super.edit(entity);
         resp.isSuccessful(true)
-                    .withStatus(Response.Status.OK);        
-               
+                .withStatus(Response.Status.OK);
+
         return resp.build();
     }
 
     @DELETE
     @Path("{id}")
     public Response remove(@PathParam("id") Integer id) {
-        
-        RestResponse resp = new RestResponse(true); 
-         
+
+        RestResponse resp = new RestResponse(true);
+
         if (id == null) {
             return newError(BAD_REQUEST, "ID not present", null, "Please, insert ID first").build();
         }
-            super.remove(super.find(id));
-            resp.isSuccessful(true)
-                    .withStatus(Response.Status.OK);        
-               
+        super.remove(super.find(id));
+        resp.isSuccessful(true)
+                .withStatus(Response.Status.OK);
+
         return resp.build();
     }
 
@@ -102,14 +108,14 @@ public class SketchFacadeREST extends AbstractFacade<Sketch> {
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response find(@PathParam("id") Integer id) {
-        
-        RestResponse resp = new RestResponse(true); 
-         
+
+        RestResponse resp = new RestResponse(true);
+
         if (id == null) {
             return newError(BAD_REQUEST, "ID not present", null, "Please, insert ID first").build();
         }
-        
-        Sketch s = super.find(id);            
+
+        Sketch s = super.find(id);
         getEntityManager().merge(s);
         resp.isSuccessful(true)
                 .withStatus(Response.Status.OK);
@@ -127,16 +133,16 @@ public class SketchFacadeREST extends AbstractFacade<Sketch> {
     @Path("{from}/{to}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        
-        RestResponse resp = new RestResponse(true); 
-         
-        if (from == null || to == null) {            
+
+        RestResponse resp = new RestResponse(true);
+
+        if (from == null || to == null) {
             return newError(BAD_REQUEST, "Range not present", null, "Please, insert range first").build();
         }
-            super.findRange(new int[]{from, to});
-            resp.isSuccessful(true)
-                    .withStatus(Response.Status.OK);        
-               
+        super.findRange(new int[]{from, to});
+        resp.isSuccessful(true)
+                .withStatus(Response.Status.OK);
+
         return resp.build();
     }
 
@@ -146,32 +152,32 @@ public class SketchFacadeREST extends AbstractFacade<Sketch> {
     public Response countREST() {
         RestResponse resp = new RestResponse(true);
         resp.isSuccessful(true)
-                    .withStatus(Response.Status.OK)       
-                    .withAttribute("value", String.valueOf(super.count()));
+                .withStatus(Response.Status.OK)
+                .withAttribute("value", String.valueOf(super.count()));
         return resp.build();
     }
-    
+
     @GET
     @Path("top")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response topScore() {
-                
-            Query q =em.createQuery("SELECT s FROM Sketch s ORDER By s.score DESC");
-            q.setMaxResults(5);  
-            
+
+        Query q = em.createQuery("SELECT s FROM Sketch s ORDER By s.score DESC");
+        q.setMaxResults(5);
+
         return ResponseFactory.newList(q.getResultList()).build();
     }
 
     @GET
     @Path("latest")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response latest() {        
-        
-        Query q =em.createQuery("SELECT s FROM Sketch s ORDER By s.createdat DESC");
+    public Response latest() {
+
+        Query q = em.createQuery("SELECT s FROM Sketch s ORDER By s.createdat DESC");
         q.setMaxResults(5);
-        
-        return ResponseFactory.newList(q.getResultList()).build();  
-                   
+
+        return ResponseFactory.newList(q.getResultList()).build();
+
     }
 
     @GET
@@ -179,22 +185,21 @@ public class SketchFacadeREST extends AbstractFacade<Sketch> {
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response findBetweenDates(@QueryParam("from") String from, @QueryParam("to") String to) {
-        
-        
-        if (from == null || to == null) {            
+
+        if (from == null || to == null) {
             return newError(BAD_REQUEST, "Range not present", null, "Please, insert range first").build();
         }
-        
+
         Query q = em.createQuery("SELECT s FROM Sketch s WHERE s.createdat BETWEEN :from AND :to ORDER BY s.createdat DESC");
         q.setParameter("from", from);
         q.setParameter("to", to);
-        
-        return ResponseFactory.newList(q.getResultList()).build(); 
+
+        return ResponseFactory.newList(q.getResultList()).build();
     }
-    
+
     @Override
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
 }
